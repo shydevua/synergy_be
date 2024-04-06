ab064130e956565130388aeaa5f830a79c509bee
Shyrinha Vladyslav <v.shyrinha@favbet.tech>
2024-04-06 11:36:52 +0200

    feat: anketa

---
+++ src/api/anketa/routes/anketa.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * anketa router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::anketa.anketa');
---
+++ src/api/anketa/services/anketa.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * anketa service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::anketa.anketa');
