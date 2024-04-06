20dbb90c0c776798eb1aa8e2fbb885bf01e77875
Shyrinha Vladyslav <v.shyrinha@favbet.tech>
2024-04-06 12:44:36 +0200

    remove cors

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,16 +1,7 @@
 module.exports = [
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
   'strapi::poweredBy',
-  {
-    name: 'strapi::cors',
-    config: {
-      enabled: true,
-      headers: '*',
-      origin: ['*'],
-    }
-  },
   'strapi::logger',
   'strapi::query',
   'strapi::body',
